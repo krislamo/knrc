0893987235ce02329c87ea000fb97eb0688bea54
Kris Lamoureux <kris@lamoureux.io>
2021-11-03 02:34:51 -0400

    Exercise 1-3. print a heading above the table

--- 02-vars-and-math.c
+++ 02-vars-and-math.c
@@ -5,6 +5,7 @@ int main()
 {
 	int fahr;
 
+	printf("Fahrenheit\tCelsius\n=======================\n");
 	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+		printf("%3d\t\t%7.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
