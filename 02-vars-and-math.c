588969b09fabab1e91ff4f4b1c37e87fc23cf76b
Kris Lamoureux <kris@lamoureux.io>
2021-11-05 20:29:35 -0400

    Exercise 1-5. print the table in reverse order

--- 02-vars-and-math.c
+++ 02-vars-and-math.c
@@ -6,6 +6,6 @@ int main()
 	int fahr;
 
 	printf("Fahrenheit\tCelsius\n=======================\n");
-	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
 		printf("%3d\t\t%7.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
