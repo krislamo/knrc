3512ae49abfa0487169f53bca7beb317dd9be073
Kris Lamoureux <kris@lamoureux.io>
2021-11-09 00:20:01 -0500

    Character counting example v2

--- 05-char-counting.c
+++ 05-char-counting.c
@@ -2,10 +2,9 @@
 
 int main()
 {
-	long nc;
+	double nc;
 
-	nc = 0;
-	while (getchar() != EOF)
-		++nc;
-	printf("%ld\n", nc);
+	for (nc = 0; getchar() != EOF; ++nc)
+		;
+	printf("%.0f\n", nc);
 }
