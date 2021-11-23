7825fcd167d77286c3c3920d21c0a9a2eef6fd73
Kris Lamoureux <kris@lamoureux.io>
2021-11-23 00:52:13 -0500

    Exercise 1-15. Make temp. conversion function

--- 02-vars-and-math.c
+++ 02-vars-and-math.c
@@ -4,6 +4,8 @@
 #define  UPPER  300
 #define  STEP   20
 
+float convert(int f);
+
 /* print Fahrenheit-Celsius table */
 int main()
 {
@@ -11,5 +13,10 @@ int main()
 
 	printf("Fahrenheit\tCelsius\n=======================\n");
 	for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
-		printf("%3d\t\t%7.1f\n", fahr, (5.0/9.0)*(fahr-32));
+		printf("%3d\t\t%7.1f\n", fahr, convert(fahr));
+}
+
+float convert(int fahr)
+{
+	return (5.0/9.0)*(fahr-32);
 }
